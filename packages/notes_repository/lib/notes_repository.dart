d75825d8786f717621f9f61f5b4e3fb5b3c58824
Grzesiek <gzaber@tlen.pl>
2022-08-07 19:12:57 +0200

    Develop notes_repository

--- packages/notes_repository/lib/notes_repository.dart
+++ packages/notes_repository/lib/notes_repository.dart
@@ -1,3 +1,4 @@
 library notes_repository;
 
 export 'src/notes_repository.dart';
+export 'package:notes_api/notes_api.dart' show Note;
--- packages/notes_repository/lib/src/notes_repository.dart
+++ packages/notes_repository/lib/src/notes_repository.dart
@@ -1,1 +1,17 @@
+import 'package:notes_api/notes_api.dart';
 
+class NotesRepository {
+  const NotesRepository({
+    required NotesApi notesApi,
+  }) : _notesApi = notesApi;
+
+  final NotesApi _notesApi;
+
+  Future<void> addNote(Note note) async => await _notesApi.addNote(note);
+  Future<void> updateNote(Note note) async => await _notesApi.updateNote(note);
+  Future<void> deleteNote(String id) async => await _notesApi.deleteNote(id);
+  Future<List<Note>> getAllNotes() async => await _notesApi.getAllNotes();
+  Future<Note?> getNote(String id) async => await _notesApi.getNote(id);
+  Future<List<Note>> searchNotes(String pattern) async =>
+      await _notesApi.searchNotes(pattern);
+}
