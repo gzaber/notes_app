573d1feb904d05d2e726989018292bfa60e2da15
Grzesiek <gzaber@tlen.pl>
2022-08-11 18:01:25 +0200

    Develop notes_overview_cubit

---
+++ lib/notes_overview/cubit/notes_overview_cubit.dart
@@ -0,0 +1,48 @@
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:notes_repository/notes_repository.dart';
+
+part 'notes_overview_state.dart';
+
+class NotesOverviewCubit extends Cubit<NotesOverviewState> {
+  NotesOverviewCubit(this._notesRepository) : super(const NotesOverviewState());
+
+  final NotesRepository _notesRepository;
+
+  void getNotes() async {
+    emit(state.copyWith(status: NotesOverviewStatus.loading));
+    try {
+      final notes = await _notesRepository.getAllNotes();
+      emit(
+        state.copyWith(
+          status: NotesOverviewStatus.success,
+          notes: notes,
+        ),
+      );
+    } catch (e) {
+      emit(
+        state.copyWith(
+          status: NotesOverviewStatus.failure,
+          errorMessage: e.toString(),
+        ),
+      );
+    }
+  }
+
+  void deleteNotes(String id) async {
+    emit(state.copyWith(status: NotesOverviewStatus.loading));
+    try {
+      await _notesRepository.deleteNote(id);
+      emit(
+        state.copyWith(status: NotesOverviewStatus.success),
+      );
+    } catch (e) {
+      emit(
+        state.copyWith(
+          status: NotesOverviewStatus.failure,
+          errorMessage: e.toString(),
+        ),
+      );
+    }
+  }
+}
