35c11ee45bb95bdeaa949b9983b70e248556a7ac
Grzesiek <gzaber@tlen.pl>
2022-08-12 19:09:54 +0200

    Refactor App - add AppView

--- lib/app/app.dart
+++ lib/app/app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:notes_app/common/common.dart';
 import 'package:notes_app/notes_overview/notes_overview.dart';
 import 'package:notes_repository/notes_repository.dart';
 
@@ -15,7 +16,37 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return RepositoryProvider.value(
       value: notesRepository,
-      child: const NotesOverviewPage(),
+      child: const AppView(),
+    );
+  }
+}
+
+class AppView extends StatelessWidget {
+  const AppView({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Notes App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: Colors.black,
+        appBarTheme: AppBarTheme(
+          backgroundColor: Colors.black,
+          toolbarHeight: 80.0,
+          elevation: 0.0,
+          centerTitle: false,
+          titleTextStyle: Theme.of(context)
+              .textTheme
+              .headlineLarge!
+              .copyWith(color: Colors.white),
+        ),
+        scaffoldBackgroundColor: Colors.black,
+        floatingActionButtonTheme:
+            const FloatingActionButtonThemeData(backgroundColor: kButtonColor),
+      ),
+      home: const NotesOverviewPage(),
     );
   }
 }
