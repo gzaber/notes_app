245a539f425ab1316fbaf5ac79740601058fe503
Grzesiek <gzaber@tlen.pl>
2022-08-13 10:52:44 +0200

    NotesApi - change addNote to createNote

--- packages/notes_api/lib/src/notes_api.dart
+++ packages/notes_api/lib/src/notes_api.dart
@@ -1,7 +1,7 @@
 import 'package:notes_api/notes_api.dart';
 
 abstract class NotesApi {
-  Future<void> addNote(Note note);
+  Future<void> createNote(Note note);
   Future<void> updateNote(Note note);
   Future<void> deleteNote(String id);
   Future<List<Note>> getAllNotes();
