e569957e85517b591ec57ec9ff8d277262519a5f
Grzesiek <gzaber@tlen.pl>
2022-08-13 10:51:25 +0200

    NotesRepository - refactor getNote to non-nullable

--- packages/notes_repository/lib/src/notes_repository.dart
+++ packages/notes_repository/lib/src/notes_repository.dart
@@ -11,7 +11,7 @@ class NotesRepository {
   Future<void> updateNote(Note note) async => await _notesApi.updateNote(note);
   Future<void> deleteNote(String id) async => await _notesApi.deleteNote(id);
   Future<List<Note>> getAllNotes() async => await _notesApi.getAllNotes();
-  Future<Note?> getNote(String id) async => await _notesApi.getNote(id);
+  Future<Note> getNote(String id) async => await _notesApi.getNote(id);
   Future<List<Note>> searchNotes(String pattern) async =>
       await _notesApi.searchNotes(pattern);
 }
