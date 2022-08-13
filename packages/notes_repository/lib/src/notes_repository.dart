23780e658cbbf326a5ddc1937e8eb720401ad759
Grzesiek <gzaber@tlen.pl>
2022-08-13 10:55:05 +0200

    NotesRepository - rename addNote to createNote

--- packages/notes_repository/lib/src/notes_repository.dart
+++ packages/notes_repository/lib/src/notes_repository.dart
@@ -7,7 +7,7 @@ class NotesRepository {
 
   final NotesApi _notesApi;
 
-  Future<void> addNote(Note note) async => await _notesApi.addNote(note);
+  Future<void> createNote(Note note) async => await _notesApi.createNote(note);
   Future<void> updateNote(Note note) async => await _notesApi.updateNote(note);
   Future<void> deleteNote(String id) async => await _notesApi.deleteNote(id);
   Future<List<Note>> getAllNotes() async => await _notesApi.getAllNotes();
