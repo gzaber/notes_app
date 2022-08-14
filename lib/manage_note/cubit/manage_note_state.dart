330b01a9070270029bb556061c8100f9abb2a43b
Grzesiek <gzaber@tlen.pl>
2022-08-14 22:45:25 +0200

    ManageNoteState - add create/update mode field

--- lib/manage_note/cubit/manage_note_state.dart
+++ lib/manage_note/cubit/manage_note_state.dart
@@ -2,19 +2,23 @@ part of 'manage_note_cubit.dart';
 
 enum ManageNoteStatus { initial, loading, empty, success, failure }
 
+enum ManageNoteMode { create, update }
+
 class ManageNoteState extends Equatable {
   ManageNoteState({
     this.status = ManageNoteStatus.initial,
     Note? note,
     this.errorMessage = '',
-  }) : note = note ?? Note(title: '');
+  })  : note = note ?? Note(title: ''),
+        mode = note == null ? ManageNoteMode.create : ManageNoteMode.update;
 
   final ManageNoteStatus status;
+  final ManageNoteMode mode;
   final Note note;
   final String errorMessage;
 
   @override
-  List<Object?> get props => [status, note, errorMessage];
+  List<Object> get props => [status, mode, note, errorMessage];
 
   ManageNoteState copyWith({
     ManageNoteStatus? status,
