d386169173058ab9562c37a7e3c4a691bf6bf3f9
Grzesiek <gzaber@tlen.pl>
2022-08-15 16:28:09 +0200

    CustomElevatedButton - improve widget

--- lib/common/widgets/custom_elevated_button.dart
+++ lib/common/widgets/custom_elevated_button.dart
@@ -2,11 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/common/common.dart';
 
 class CustomElevatedButton extends StatelessWidget {
-  final IconData icon;
+  final Widget widget;
   final Function() onPressed;
 
   const CustomElevatedButton({
-    required this.icon,
+    required this.widget,
     required this.onPressed,
     Key? key,
   }) : super(key: key);
@@ -18,8 +18,12 @@ class CustomElevatedButton extends StatelessWidget {
           const EdgeInsets.fromLTRB(kSidePadding, 16.0, kSidePadding, 16.0),
       child: ElevatedButton(
         onPressed: onPressed,
-        child: Icon(icon, size: 30.0),
-        style: ElevatedButton.styleFrom(primary: kButtonColor),
+        child: widget,
+        style: ElevatedButton.styleFrom(
+          primary: kButtonColor,
+          shape:
+              RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        ),
       ),
     );
   }
